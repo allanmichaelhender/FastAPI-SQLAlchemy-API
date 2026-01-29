71be89a713ac962df6ac271fb84369e3d35b84f3
Allan <allanhender@gmail.com>
2026-01-29 18:22:09 +0000

    adding database model

--- myapi.py
+++ myapi.py
@@ -8,8 +8,33 @@ from typing import Optional, List
 
 app = FastAPI(title="Integration with SQL")
 
-#Endpoints
+#Database setuo
+engine = create_engine("sqlite:///users.db", connect_args={"check_same_thread":False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
+#Database Model
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True,  index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    role = Column(String(100), nullable=False)
+
+Base.metadata.create_all(engine)
+
+#Pydantic Models
+class UserCreate(BaseModel):
+    name:str
+    email:str
+    role:str
+
+class UserResponse
+
+
+
+#Endpoints
 @app.get("/")
 def root():
     return {"message": "Intro to FastAPI x SQLAlchemy"}
